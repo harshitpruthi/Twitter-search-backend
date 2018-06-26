ae21156af7a5b24f2986ba13f7a388ec9e3c241d
harshitpruthi <harshitpruthi23@gmail.com>
2018-06-26 14:53:49 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+var appsettings = {
+    consumerkey: 'hX6hBRPzIaEBnLHyYTMZWjhTS',
+    consumersecret: 'ygR4B3ysidrAg5us253VLw50pl2DVAwoV6k5Y6H2WRjlb9ZaHa',
+    bearertoken: ''
+};
+
+module.exports = appsettings;
