eb1517a5e69fc024ff9799611fcaef621ccbe611
Harshit Pruthi <harshitpruthi23@gmail.com>
2018-08-13 11:39:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var appsettings = {
-    consumerkey: 'hX6hBRPzIaEBnLHyYTMZWjhTS',
-    consumersecret: 'ygR4B3ysidrAg5us253VLw50pl2DVAwoV6k5Y6H2WRjlb9ZaHa',
+    consumerkey: 'Enter your consumer Key',
+    consumersecret: 'Enter your consumer secret key',
     bearertoken: ''
 };
 
